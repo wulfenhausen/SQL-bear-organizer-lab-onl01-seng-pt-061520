2feefe0a5ef50c842c414337c39f281e254f1fe6
William Han Findiesen <william.findiesen@gmail.com>
2020-08-24 14:56:55 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -3,5 +3,6 @@ INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy"
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 6, "female", "red","sassy",1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 9, "male", "black","aloof",1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 11, "female", "white","stoic",1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 8, "male", "green","mischevious",1);
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 100, "female", "black","not all that lively", 0);
 INSERT INTO bears (age, gender, color, temperament, alive) VALUES (4, "female", "black","hungry", 1);
